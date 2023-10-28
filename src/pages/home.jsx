fb35ea0bf57976cc6e1e18196035395d2577ffcb
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-10-29 00:24:08 +0700

    fix padding button

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,11 +4,11 @@ function Home() {
   return (
     <>
       <div className="flex flex-col border-fuchsia-900 justify-center  border-2 p-3 gap-1 mt-3 rounded-md items-center w-auto mx-3 w-[50%]">
-        <button className=" border-fuchsia-900  border-2 p-3 gap-3  rounded-md items-center w-40 ">
+        <button className="border-fuchsia-900  border-2 p-3 gap-3  rounded-md items-center w-40 ">
           {" "}
           Login{" "}
         </button>
-        <button className=" border-fuchsia-900   w-40 border-2 p-3  rounded-md items-center w-auto s">
+        <button className="border-fuchsia-900  border-2 p-3 gap-3  rounded-md items-center w-40 ">
           {" "}
           register{" "}
         </button>
