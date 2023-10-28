175b999ba83d1ee97c93e3f59f270fc3aa27bf64
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-10-29 00:22:41 +0700

    fix button

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function Home() {
   return (
     <>
-      <div className="flex flex-col border-fuchsia-900 justify-center  border-2 p-3 gap-1 mt-3 rounded-md items-center w-auto mx-auto w-[50%]">
-        <button className=" border-fuchsia-900   w-[70%] border-2 p-3 gap-3  rounded-md items-center w-auto ">
+      <div className="flex flex-col border-fuchsia-900 justify-center  border-2 p-3 gap-1 mt-3 rounded-md items-center w-auto mx-3 w-[50%]">
+        <button className=" border-fuchsia-900  border-2 p-3 gap-3  rounded-md items-center w-40 ">
           {" "}
           Login{" "}
         </button>
-        <button className=" border-fuchsia-900   w-[70%] border-2 p-3  rounded-md items-center w-auto s">
+        <button className=" border-fuchsia-900   w-40 border-2 p-3  rounded-md items-center w-auto s">
           {" "}
           register{" "}
         </button>
