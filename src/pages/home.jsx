c74a4c085866b3166f09cfcf80d870097f9aa591
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-10-30 10:46:52 +0700

    fix home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Button } from "flowbite-react";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
-    <>
-      <div>
-        <h1>Home</h1>
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-center">
+        <h1>Selamat Datang di Aplikasi Todo</h1>
+        <p>Ini adalah halaman beranda aplikasi todo yang sederhana.</p>
+        <p>menggunakan React-Context dan FlowbyteUI ❤️</p>
+        <div className="mt-6">
+          <Button style={{ margin: "0 auto" }}>
+            <NavLink to="/todo">Get Started</NavLink>
+          </Button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
